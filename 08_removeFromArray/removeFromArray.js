32af3ef39d0321775a5e051dd16e3b382f320463
chrissven86 <christoffer.8618@gmail.com>
2025-12-06 15:36:38 +0100

    Updated solutions after completion if lesson Loops and Arrays

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeFromArray) {
+    for (let value of removeFromArray) {
+        while (array.indexOf(value) !== -1) {
+            const index = array.indexOf(value);
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+function sumAll(a, b) {
+  // Check for negative numbers or non-integers
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return 'ERROR';
+  }
 
-};
+  let start = Math.min(a, b);
+  let end = Math.max(a, b);
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
