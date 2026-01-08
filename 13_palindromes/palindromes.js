1d56e0f93cbb09ef4453694dfbdce688f7d63ae2
chrissven86 <christoffer.8618@gmail.com>
2026-01-08 12:38:55 +0100

    upload of solutions to exercises 12 - 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphNum = 'abcdefghihjklmnopqrstuvwxyz1234567890';
+    
+    const cleanStr = string
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphNum.includes(char))
+    .join('');
 
+    const revString = cleanStr.split('').reverse().join('');
+
+    return cleanStr === revString;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+        return currentAge > oldestAge ? current : oldest;
+    });
+};
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
